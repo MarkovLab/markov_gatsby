246c56b307100a6c19b4417dc32770fc84675028
Richard Kim <kimr@mit.edu>
2019-06-25 21:52:16 -0400

    modified 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
 import { Link } from 'gatsby'
 
+import { Typography } from '@material-ui/core'
 import { LandingLayout } from 'components/Layout'
 
-const NotFoundPage = () => (
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    height: '100vh',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+const NotFoundPage = ({ classes }) => (
   <LandingLayout>
-    <p>
-      Uh oh! The page you're looking for does not exist. Click{' '}
-      <Link to="/">here</Link> to go back to home page.
-    </p>
+    <div className={classes.container}>
+      <Typography variant="body1">
+        Uh oh! The page you're looking for does not exist. Click{' '}
+        <Link to="/">here</Link> to go back to home page.
+      </Typography>
+    </div>
   </LandingLayout>
 )
 
-export default NotFoundPage
+export default withStyles(styles)(NotFoundPage)
